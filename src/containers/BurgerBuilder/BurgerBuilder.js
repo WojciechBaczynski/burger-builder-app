d22c263f5b028b2ac0c601a477903bbfec7bad73
Wojciech Baczyński <wp.baczynski@gmail.com>
2019-03-27 19:49:03 +0100

    Add sending dummy POST request to the firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,7 +3,8 @@ import Aux from '../../hoc/Aux/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
-import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.2,
@@ -76,7 +77,24 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContiniueHandler = () => {
-        alert('You continiue!');
+        //alert('You continiue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: "WB",
+                adress: {
+                    street: "Testst 1",
+                    zipcode: "12345",
+                    country: "Poland"
+                },
+                email: "test@test.com"
+            },
+            deliveryMethod: "fastest"
+        }
+        axios.post('/orders.json', order)
+            .then(res => console.log(res))
+            .catch(err => console.log(err));
     }
 
     render() {
