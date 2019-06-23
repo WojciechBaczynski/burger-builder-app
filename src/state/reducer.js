4db67c901a0507ceed83b151854c5362d42820fa
WojciechBaczynski <wp.baczynski@gmail.com>
2019-06-23 18:35:02 +0200

    Create redux structure

---
+++ src/state/reducer.js
@@ -0,0 +1,12 @@
+import * as actionTypes from './actions'
+
+const initialState = {
+    ingredients: null,
+    totalPrice: 3
+}
+
+const Reducer = (state = initialState, action) => {
+
+}
+
+export default Reducer
